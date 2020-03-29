67f45e89942eab633a30aaf3ecec5d7820f2e350
phongnguyen39 <phong.nguyen39@gmail.com>
2020-03-28 17:26:56 -0700

    commented out bad jax code until ready

--- freecodecamp/src/index.js
+++ freecodecamp/src/index.js
@@ -70,8 +70,8 @@ class CompTypes extends React.Component {
     return (
       <div className ='p_components'>
         <h3>Different types of components in React</h3>
-        {/* <h5 className='c_components' >pure JSX element syntax <pre><code>const JsxELement = </code></pre></h5> */}
-        <h5 className='c_components' onClick={jax}>pure JSX element syntax {jax}</h5>
+        <h5 className='c_components' >pure JSX element syntax <pre><code>const JsxELement = </code></pre></h5>
+        {/* <h5 className='c_components' onClick={jax}>pure JSX element syntax {jax}</h5> */}
         <h5 className='c_components'>Stateless Functional component syntax <pre><code>const FunctionalComponent =()=> {}</code></pre></h5>
         <h5 className='c_components'>ES6 class component syntax <pre><code>class Es6ClassComponent extends React.Component{}</code></pre></h5>
       </div>
