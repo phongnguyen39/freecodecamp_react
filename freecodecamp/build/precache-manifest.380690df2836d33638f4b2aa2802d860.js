708b11848c99bff1fd4ae8b5d806fbad52314b0b
phongnguyen39 <phong.nguyen39@gmail.com>
2020-04-05 22:16:53 -0700

    commented out build folder from git.ignore

---
+++ freecodecamp/build/precache-manifest.380690df2836d33638f4b2aa2802d860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7312f984b6cfb4894ed692c370612b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54cc3b9df2fbcebfef14",
+    "url": "/static/css/main.c311ac25.chunk.css"
+  },
+  {
+    "revision": "a0b172a3bbb374ce17a9",
+    "url": "/static/js/2.c8543d49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c8543d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cc3b9df2fbcebfef14",
+    "url": "/static/js/main.89dd41ae.chunk.js"
+  },
+  {
+    "revision": "951449f96295a2064394",
+    "url": "/static/js/runtime-main.3ab09ae5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
