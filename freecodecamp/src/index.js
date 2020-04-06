f705b7b8642e5a76a4df6031dcce052c207470dd
phongnguyen39 <phong.nguyen39@gmail.com>
2020-04-05 20:52:50 -0700

    Getting into Stateful components

--- freecodecamp/src/index.js
+++ freecodecamp/src/index.js
@@ -7,8 +7,8 @@ import * as serviceWorker from './serviceWorker';
 
 const github = <a href='https://www.github.com' target='_blank' >Github</a>
 
-const URLs =()=> {
-  return(
+const URLs = () => {
+  return (
     <div>
       <a href='https://www.github.com' target='_blank' >Github</a>
       <a href='https://www.linkedin.com/in/phongnguyenmha' target='_blank' >Linkedin</a>
@@ -33,15 +33,15 @@ class Footer extends React.Component {
 }
 
 // class SVGs extends React.Component {
-  
+
 //   constructor(props){
 //     super(props);
 //   }
-  
+
 //   render(){
 //     return (
 //       <div>
-      
+
 //         <svg>
 //           <circle cx='50' cy='50' r='20' stroke='green' stroke-width='1' fill='yellow' />
 //         </svg>
@@ -86,12 +86,12 @@ const MyComponent = function () {
 }
 
 class CompTypes extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
   render() {
     return (
-      <div className ='p_components'>
+      <div className='p_components'>
         <h3>Different types of components in React</h3>
         <h5 className='c_components' >pure JSX element syntax <pre><code>const JsxELement = </code></pre></h5>
         {/* <h5 className='c_components' onClick={jax}>pure JSX element syntax {jax}</h5> */}
@@ -121,7 +121,7 @@ const ParentComponent = () => {
 }
 
 const CurrentDate = (props) => {
-  return(
+  return (
     <div className='c_components'>
       <p>Current Date: {props.date}</p>
     </div>
@@ -131,31 +131,31 @@ const CurrentDate = (props) => {
 
 
 
-const DaysOfWeek =(props)=>{
-  return(
-    <div className = 'c_components'>
+const DaysOfWeek = (props) => {
+  return (
+    <div className='c_components'>
       <p>{props.dow.join(' | ')}</p>
     </div>
   )
 }
 
 class Calendar extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
   render() {
-    return(
+    return (
       <div className='p_components'>
         <h3>This is an example of passing properties between components</h3>
         <h3>Calendar Component</h3>
-        <CurrentDate date={Date()}/>
+        <CurrentDate date={Date()} />
         <p>This Calendar component consist of two parts:</p>
         <ul>
           <li>ES6 class component</li>
           <li>Stateless functional component</li>
-          </ul>
-          <p>The ES6 class component has the constructor, super, and props.  .   The stateless functional component has the props and date attribute.  It is nested in the ES6 component.</p>
-          <DaysOfWeek dow = {["Mo","Tu","We","Th","Fr","Sa","Su"]} />
+        </ul>
+        <p>The ES6 class component has the constructor, super, and props.  .   The stateless functional component has the props and date attribute.  It is nested in the ES6 component.</p>
+        <DaysOfWeek dow={["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]} />
       </div>
     )
   }
@@ -163,10 +163,10 @@ class Calendar extends React.Component {
 
 const DefaultProp = (props) => {
   return (
-    <div className = 'p_components'>
+    <div className='p_components'>
       <p>Default Prop</p>
       <p>.defaultProps passes a default value as a prop through a component if none is provided.   It can be overidden when another value is explicitly passed.</p>
-      <p className = 'c_components'>{props.someKey}</p>
+      <p className='c_components'>{props.someKey}</p>
       <p>Type something below to override the default</p>
     </div>
   )
@@ -180,9 +180,9 @@ class UsingThis extends React.Component {
   constructor(props) {
     super(props);
   }
-  render(){
+  render() {
     return (
-      <div className = 'p_components'>
+      <div className='p_components'>
         <h3>Passing Props from ES6 Class Component to ES6 Class Components</h3>
         <p>Previous examples have been passing from ES6 Class to stateless function components</p>
         <p>Major difference is the use of "this" in front of the prop. </p>
@@ -192,6 +192,41 @@ class UsingThis extends React.Component {
   }
 }
 
+const AboutState = () => {
+  return (
+    <div className='p_components'>
+      
+        <p>A stateless functional component is any function you write which accepts props and returns JSX.</p>
+        <p>A stateless component, on the other hand, is a class that extends React.Component, but does not use internal state (covered in the next challenge).</p> 
+        <p>Finally, a stateful component is a class component that does maintain its own internal state. You may see stateful components referred to simply as components or React components.</p>
+      <a href='https://www.freecodecamp.org/learn/front-end-libraries/react/review-using-props-with-stateless-functional-components'>Ref: freecodecamp</a>
+    </div>
+  )
+}
+
+class FirstStatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      stateType: 'Stateful with this.state.data written directly into the return statement',
+      stateType2: 'Stateful with this.state.data written in the render before the return statement. Writing it this way allows you to perform Javascript functions.  The this.state.data is assigned to avariable without curly braces'
+    }
+  }
+  
+  render() {
+    return (
+    <div className ='p_components'>
+      <h3>Stateful Components</h3>
+    <p>State syntax is within the constructor.  this.state = key value object</p>
+      <p className= 'c_components'>{this.state.stateType}</p>
+      <p>The state is encapsulated to it's own component.  It can be passed as props to another component though.</p>
+      <p>When the state data of a component is updated then it updates the virtual DOM which then updates the DOM.  This allows the UI to change only where needed</p>
+      <p className= 'c_components'>{this.state.stateType2}</p>
+
+    </div>
+    )
+  }
+}
 
 
 
@@ -204,17 +239,20 @@ class MainApp extends React.Component {
     return (
       <div className='container'>
         <div>
-          <h1 className = 'header'>React Playground</h1>
+          <h1 className='header'>React Playground</h1>
           <a href='https://github.com/phongnguyen39/freecodecamp_react' alt="Github link to readme" target='_blank'>For full notes see Github repo</a>
           {/* <SVGs /> */}
           <Descriptions array={['rate', 'fart']} />
           <CompTypes />
           <ParentComponent />
           {/* <PassingProps /> */}
-          <Calendar  />
+          <Calendar />
           <DefaultProp />
-          <DefaultProp someKey='Overidden Prop'/>
+          <DefaultProp someKey='Overidden Prop' />
           <UsingThis thisExample='this.props.data' />
+          <AboutState />
+          <FirstStatefulComponent />
+
           <Footer />
         </div>
       </div>
