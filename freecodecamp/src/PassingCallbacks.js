a79b4d4d8c958087f1eb78fec06d7a38e67230b8
phongnguyen39 <phong.nguyen39@gmail.com>
2020-04-26 10:56:17 -0700

    completed another section of the React course

--- freecodecamp/src/PassingCallbacks.js
+++ freecodecamp/src/PassingCallbacks.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import { Component } from 'react';
 
+class GetInput extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <h4>Get Input:</h4>
+                <input value={this.props.input}
+                    onChange={this.props.handleChange} />
+            </div>
+        )
+    }
+}
+
+class RenderInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h4>Input Render:</h4>
+                <p>{this.props.input}</p>
+            </div>
+        )
+    }
+}
+
 class PassingCallBacks extends React.Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            input: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
     }
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        })
+    }
+
 
     render() {
         return (
             <div className='p_components'>
                 <h3>Passing Callback Methods as Props</h3>
+                <p>We can pass handler functions or methods to child components from the parent component too.  These functions are passed as this.props.</p>
+                <GetInput input={this.state.input} handleChange={this.handleChange} />
+                <RenderInput input={this.state.input} />
             </div>
         )
     }
 }
+
 
 export default PassingCallBacks;
