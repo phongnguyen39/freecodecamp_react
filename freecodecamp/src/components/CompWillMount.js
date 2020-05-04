78fe1f4b16cd9a6ee61dfeab983940a2eae78fbb
phongnguyen39 <phong.nguyen39@gmail.com>
2020-05-03 18:54:39 -0700

    experimenting with Javascript timestamps in the CompWillMount.js

--- freecodecamp/src/components/CompWillMount.js
+++ freecodecamp/src/components/CompWillMount.js
@@ -4,18 +4,33 @@ import {Component} from 'react';
 class CompWillMount extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            initialTime: '',
+            currentTime: ''
+        }
     }
 
     componentWillMount(){
-        console.log("Mounted")
+        let date = new Date();
+        console.log("Mounted", date.toString())
+        this.setInitialTime(date);
+    }
+    
+    setInitialTime(date){
+        this.setState=({
+            currentTime: date.toString()
+        })
     }
 
     render() {
+        console.log(this.state)
         return(
             <div className='p_components'>
                 <h3>componentWillMount()</h3>
                 <p>Method is called before the render() method when a component is being mounted to the DOM</p>
-         
+                <p>This is demonstrated by </p>
+                <p>Initial Timestamp: {this.state.initialTime}</p>
+                <p>Current Timestamp: {this.state.currentTime}</p>
             </div>
         )
     }
