fbc5a521fd1df8df8cd5e512c18b044f40727391
phongnguyen39 <phong.nguyen39@gmail.com>
2020-05-03 19:06:23 -0700

    checkpoint

--- freecodecamp/src/components/CompWillMount.js
+++ freecodecamp/src/components/CompWillMount.js
@@ -4,6 +4,7 @@ import {Component} from 'react';
 class CompWillMount extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             initialTime: '',
             currentTime: ''
@@ -12,15 +13,18 @@ class CompWillMount extends React.Component {
 
     componentWillMount(){
         let date = new Date();
-        console.log("Mounted", date.toString())
-        this.setInitialTime(date);
+        console.log("Mounted", date)
+        // let dateStr1 = date.toString();
+        // this.setInitialTime(dateStr1);
     }
     
-    setInitialTime(date){
-        this.setState=({
-            currentTime: date.toString()
-        })
-    }
+    // setInitialTime(){
+    //     let date1 = new Date();
+    //     console.log("Mountedsdfsdfsdf", date1.toString())
+    //     this.setState=({
+    //         currentTime: ''
+    //     })
+    // }
 
     render() {
         console.log(this.state)
@@ -28,9 +32,9 @@ class CompWillMount extends React.Component {
             <div className='p_components'>
                 <h3>componentWillMount()</h3>
                 <p>Method is called before the render() method when a component is being mounted to the DOM</p>
-                <p>This is demonstrated by </p>
-                <p>Initial Timestamp: {this.state.initialTime}</p>
-                <p>Current Timestamp: {this.state.currentTime}</p>
+                <p>This is demonstrated by inspecting the web browser, there will be a timestamp of when the componentWillMount() executed in the console log. A couple of seconds would have now.  The lifecycle hook executed as the virtual DOM was mounted to the HTML DOM</p>
+                {/* <p>Initial Timestamp: {this.state.initialTime}</p>
+                <p>Current Timestamp: {this.state.currentTime}</p> */}
             </div>
         )
     }
